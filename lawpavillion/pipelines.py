c2bbb7d8178911924fe0ec746b0ee87ae3a58d24
codewithpatch <chesterdealday@gmail.com>
2020-04-22 20:16:35 +0800

    Added Class to populate postgres database

--- lawpavillion/pipelines.py
+++ lawpavillion/pipelines.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
+import logging
+import json
 import socket
 
 import pymongo
 from scrapy.utils.project import get_project_settings
+
+from sqlalchemy.orm import sessionmaker
+
+from lawpavillion.models import (
+    connect_db, create_schema, create_tables,
+    Case, Court, Jurisdiction, Casebody, Attorney, Opinion, Judge, Ratio_decidendi
+)
 
 
 class LawpavillionPipeline(object):
@@ -59,3 +68,203 @@ class LawpavillionPipeline(object):
 class TestPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class PostgresDBPipeline(object):
+    def __init__(self):
+        engine = connect_db()
+        create_schema(engine, "law_pavillion")
+        create_tables("law_pavillion")
+        self.session = sessionmaker(bind=engine)
+
+    def process_item(self, item, spider):
+        session = self.session()
+
+        # Process Case
+        try:
+            # Create case object with basic fields
+            case = Case(
+                id=item['id'],
+                url=item['url'],
+                name=item['name'],
+                name_abbreviation=item['name_abbreviation'],
+                slug=item['slug'],
+                suit_no=item['suit_no'],
+                decision_date=item['decision_date'],
+                frontend=item['frontend'],
+                citations=item['citations'],
+            )
+            session.add(case)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            session.close()
+            logging.error(e)
+            return item
+
+        # Process Jurisdiction
+        try:
+            # Check if jurisdiction already exsists
+            jurisdiction = session.query(Jurisdiction).filter(
+                Jurisdiction.name_long == item['jurisdiction']['name_long']
+            ).filter(
+                Jurisdiction.name == item['jurisdiction']['name']
+            )
+            if not jurisdiction:
+                jurisdiction = Jurisdiction(
+                    id=item['jurisdiction']['id'],
+                    slug=item['jurisdiction']['slug'],
+                    whitelisted=item['jurisdiction']['whitelisted'],
+                    name_long=item['jurisdiction']['name_long'],
+                    name=item['jurisdiction']['name'],
+                )
+                session.add(jurisdiction)
+                session.commit()
+        except Exception as e:
+            session.rollback()
+            session.close()
+            logging.error(e)
+            return item
+
+        # Property Court
+        try:
+            # Check if court already exists
+            court = session.query(Court).filter(
+                Court.name == item['court']['name']
+            ).filter(
+                Court.name_abbreviation == item['court']['name_abbreviation']
+            ).filter(
+                Court.slug == item['court']['slug']
+            ).filter(
+                Court.id == item['court']['id']
+            )
+            if not court:
+                court = Court(
+                    name=item['court']['name'],
+                    name_abbreviation=item['court']['name_abbreviation'],
+                    slug=item['court']['slug'],
+                    id=item['court']['id']
+                )
+                session.add(court)
+                session.commit()
+        except Exception as e:
+            session.rollback()
+            session.close()
+            logging.error(e)
+            return item
+
+        # Process Casebody
+        try:
+            # Check if casebody already exists
+            casebody = session.query(Casebody).filter(
+                Casebody.summary == item['casebody']['summary']
+            )
+            if not casebody:
+                casebody = Casebody(
+                    id=item['casebody']['id'],
+                    summary=item['casebody']['summary'],
+                    respondents=item['casebody']['respondents'],
+                )
+                session.add(casebody)
+                session.commit()
+
+        except Exception as e:
+            session.rollback()
+            session.close()
+            logging.error(e)
+            return item
+
+        # Process Attorney
+        try:
+            # Check if attorney already exsists
+            for name in item['attorneys']:
+                attorney = session.query(Attorney).filter(
+                    Attorney.name == name
+                )
+                if not attorney:
+                    attorney = Attorney(
+                        name=name,
+                        title='To setup'
+                    )
+                    session.add(attorney)
+                    session.commit()
+
+        except Exception as e:
+            session.rollback()
+            session.close()
+            logging.error(e)
+            return item
+
+        # Process Opinion
+        try:
+            # Check if opinion already exists
+            opinions = item['casebody'].get('opinions')
+            if opinions:
+                for opinion_hash in opinions:
+                    opinion = session.query(Opinion).filter(
+                        Opinion.author == opinion_hash['author']
+                    ).filter(
+                        Opinion.text == opinion_hash['text']
+                    )
+                    if not opinion:
+                        opinion = Opinion(
+                            author = opinion_hash['author'],
+                            text = opinion_hash['text']
+                        )
+                        session.add(opinion)
+                        session.commit()
+        except Exception as e:
+            session.rollback()
+            session.close()
+            logging.error(e)
+            return item
+
+        # Process Judge
+        try:
+            # Check if judge already exists
+            judges = item['casebody'].get('judges')
+            for judge_hash in judges:
+                judge = session.query(Judge).filter(
+                    Judge.name == judge_hash.get('name')
+                ).filter(
+                    Judge.title == judge_hash.get('title')
+                )
+                if not judge:
+                    judge = Judge(
+                        name=judge_hash.get('name'),
+                        title=judge_hash.get('title')
+                    )
+                    session.add(judge)
+                    session.commit()
+        except Exception as e:
+            session.rollback()
+            session.close()
+            logging.error(e)
+            return item
+
+        # Process Ratio decidendi
+        try:
+            # Check if ratio decidendi already exists
+            ratio_decidendis = item['casebody'].get('ratio_decidendi')
+            for decidendi_hash in ratio_decidendis:
+                ratio_decidendi = session.query(Ratio_decidendi).filter(
+                    Ratio_decidendi.matter == decidendi_hash.get('matter')
+                ).filter(
+                    Ratio_decidendi.topic == decidendi_hash.get('topic')
+                ).filter(
+                    Ratio_decidendi.text == decidendi_hash.get('text')
+                )
+
+                if not ratio_decidendi:
+                    ratio_decidendi = Ratio_decidendi(
+                        matter=decidendi_hash.get('matter'),
+                        topic=decidendi_hash.get('topic'),
+                        text=decidendi_hash.get('text')
+                    )
+        except Exception as e:
+            session.rollback()
+            session.close()
+            logging.error(e)
+
+
+
